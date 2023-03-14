a2c180423da161f70570b801f9b5bc5681420880
Awxsl <Awxslu@gmail.com>
2023-03-14 04:06:50 +0300

    Initialized the project and added the routes to different pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Events from './pages/Events'
+import Users from './pages/Users'
+import Event from './pages/Event'
+import User from './pages/User'
+import Profile from './pages/Profile'
+import Notifications from './pages/Notifications'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/signin' element={<SignIn/>} />
+          <Route path='/signup' element={<SignUp/>} />
+          <Route path='/events' element={<Events/>} />
+          <Route path='/users' element={<Users/>} />
+          <Route path='/event' element={<Event/>} />
+          <Route path='/user' element={<User/>} />
+          <Route path='/profile' element={<Profile/>} />
+          <Route path='/notifications' element={<Notifications/>} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
